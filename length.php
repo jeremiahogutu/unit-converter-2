dc588ad6ef97be95280f75e58511aa379affe6cf
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2017-09-04 14:56:11 -0400

    added convert to meters function

--- length.php
+++ length.php
@@ -1,3 +1,24 @@
+<?php
+    function convert_to_meters ($value, $from_unit) {
+        if ($from_unit == 'feet') {
+            return $value * 0.3048;
+        }
+        return "Unsupported unit.";
+    }
+
+    $from_value = '';
+    $from_unit = '';
+    $to_unit = '';
+    $to_value = '';
+
+    if ($_POST['submit']) {
+        $from_value = $_POST['from_value'];
+        $from_unit = $_POST['from_unit'];
+        $to_unit = $_POST['to_unit'];
+
+        $to_value = convert_to_meters($from_value, $from_unit);
+    }
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -15,22 +36,29 @@
         
         <div class="entry">
           <label>From:</label>&nbsp;
-          <input type="text" name="from_value" value="" />&nbsp;
+          <input type="text" name="from_value" value="<?php echo $from_value; ?>" />&nbsp;
           <select name="from_unit">
-            <option>From Unit</option>
+              <option value="inches"<?php if ($from_unit == 'inches') { echo " selected"; }?>>inches</option>
+              <option value="feet"<?php if ($from_unit == 'feet') {echo " selected"; }?>>feet</option>
+              <option value="yards"<?php if ($from_unit == 'yards') {echo " selected"; }?>>yards</option>
+              <option value="miles"<?php if ($from_unit == 'miles') {echo " selected"; }?>>miles</option>
+              <option value="millimeters"<?php if ($from_unit == 'millimeters') {echo " selected"; }?>>millimeters</option>
+              <option value="centimeters"<?php if ($from_unit == 'centimeters') {echo " selected"; }?>>centimeters</option>
+              <option value="meters"<?php if ($from_unit == 'meters') {echo " selected"; }?>>meters</option>
+              <option value="kilometers"<?php if ($from_unit == 'kilometers') {echo " selected"; }?>>kilometers</option>
           </select>
         </div>
         
         <div class="entry">
           <label>To:</label>&nbsp;
-          <input type="text" name="to_value" value="" />&nbsp;
+          <input type="text" name="to_value" value="<?php echo $to_value; ?>" />&nbsp;
           <select name="to_unit">
-            <option>To Unit</option>
+              <option value="meters"<?php if ($to_unit == 'meters') {echo " selected"; }?>>meters</option>
           </select>
           
         </div>
         
-        <input type="submit" value="Submit" />
+        <input type="submit" name="submit" value="Submit" />
       </form>
   
       <br />
